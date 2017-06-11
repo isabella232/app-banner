4df3a84bdec15a8e39c7c1e81f368d7689db15b6
Alexey Guskov <lexag@mail.ru>
2017-06-11 13:21:43 +0300

    mini mobile banner works
    Signed-off-by: Alexey Guskov <lexag@mail.ru>

--- src/components/MobileBannerMini.jsx
+++ src/components/MobileBannerMini.jsx
@@ -1,22 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
+import ElementClass from 'element-class';
 
 import style from './css/MobileBanner.scss';
 
-const MobileBannerMini = ({ app, locale }) => (
-  <div className={style.mini} id={style.AppBanner}>
-    <a href={app.url} className={style.container}>
-      <div className={style.img}>
-        <img alt={app.name} src={app.icon} role="presentation" />
+const height = 50;
+
+export default class MobileBannerMini extends Component {
+  componentDidMount() {
+    setTimeout(() => {
+      // add some space for the banner and show it
+      ElementClass(document.querySelector('html'))
+        .add(style.MiniBannerPresent);
+
+      // TODO: fix the fixed headers
+
+      // scroll to the start of the page and hide the banner
+      if (window.scrollY === 0) { // not yet scrolled
+        window.scrollTo(0, height);
+      }
+    }, 500);
+  }
+
+  render() {
+    const { app, locale } = this.props;
+    return (
+      <div className={style.mini} id={style.TheBanner}>
+        <a href={app.url} className={style.container}>
+          <div className={style.img}>
+            <img alt={app.name} src={app.icon} role="presentation" />
+          </div>
+
+          <div className={style.info}>
+            <div className={style.name}>{app.name}</div>
+            <div className={style.cta}>{locale.cta}</div>
+          </div>
+
+          <div className={`${style.btn} ${style.btn__fixed}`}>{locale.view}</div>
+        </a>
       </div>
-
-      <div className={style.info}>
-        <div className={style.name}>{app.name}</div>
-        <div className={style.cta}>{locale.cta}</div>
-      </div>
-
-      <div className={`${style.btn} ${style.btn__fixed}`}>{locale.view}</div>
-    </a>
-  </div>
-);
-
-export default MobileBannerMini;
+    );
+  }
+}
